47f043cd8e7e525445137c941870451143e3c748
vaibhav09981 <vaibhavjj99@gmail.com>
2025-06-30 22:26:19 +0530

    `Refactor index.html, script.js and style.css to remove unnecessary elements and add new functionality`

--- script.js
+++ script.js
@@ -1,14 +1,16 @@
-var tl = gsap.timeline();
+document.querySelectorAll(".reveal")
+    .forEach(function(elem){
+        let spanParent = document.createElement("span")
+        let spanChild = document.createElement("span")
+    
+        spanParent.classList.add("parent");
+        spanChild.classList.add("Child");
 
-tl
-    .to("#fs", {
-        height : 0,
-        duration: 2,
-        ease: Expo.easeInOut
-    })
-    .to("#white",{
-        height: 100%,
-        duration: 2,
-        delay: -1.6,
-        ease: Expo.easeInOut
+        spanChild.textContent = elem.textContent;
+        spanParent.appendChild(spanChild);
+
+        elem.innerHTML = "";
+        elem.appendChild(spanParent);
+        
+        
     })
