199a382f80c4ef6dbddcea6adf917db8ba73f7d1
vaibhav09981 <vaibhavjj99@gmail.com>
2025-07-16 19:12:39 +0530

    typewriter animation and a few details added

--- script.js
+++ script.js
@@ -1,16 +1,44 @@
-var toggle = document.getElementById("menu-toggle");
-var items = document.getElementById("menu-content");
-// adding check status
-updateButtonLabel();
-window.addEventListener("resize", updateButtonLabel);
-toggle === null || toggle === void 0 ? void 0 : toggle.addEventListener("click", function () {
-    items === null || items === void 0 ? void 0 : items.classList.toggle("hidden");
+document.addEventListener('DOMContentLoaded', function() {
+  const phrases = [
+    'Web developer',
+    'Front-End Developer',
+    'Marketing and PR handler'
+  ];
+  let i = 0;
+  let j = 0;
+  let currentPhrase = '';
+  let isDeleting = false;
+  const typewriterTextElement = document.getElementById('typewriter-text');
+
+  function typeWriter() {
+    if (i < phrases.length) {
+      if (!isDeleting) {
+        currentPhrase = phrases[i].substring(0, j + 1);
+        j++;
+        typewriterTextElement.innerHTML = `<strong>${currentPhrase}</strong>`;
+        if (j === phrases[i].length) {
+          isDeleting = true;
+          setTimeout(typeWriter, 500); // Pause at end of phrase
+        } else {
+          setTimeout(typeWriter, 50); // Typing speed
+        }
+      } else {
+        currentPhrase = phrases[i].substring(0, j - 1);
+        j--;
+        typewriterTextElement.textContent = currentPhrase;
+        if (j === 0) {
+          isDeleting = false;
+          i++;
+          setTimeout(typeWriter, 250); // Pause before typing next phrase
+        } else {
+          setTimeout(typeWriter, 25); // Deleting speed
+        }
+      }
+    } else {
+      i = 0; // Loop back to the beginning
+      setTimeout(typeWriter, 500); // Pause before restarting
+    }
+  }
+
+  typeWriter();
 });
-function updateButtonLabel() {
-    if (window.innerWidth <= 768) {
-        toggle.innerHTML = "☰";
-    }
-    else {
-        toggle.innerHTML = "Connect With Me";
-    }
-}
