911bd6001a6c9eff9e974d010cea90364d57ea23
vaibhav09981 <vaibhavjj99@gmail.com>
2025-10-13 13:37:44 +0530

    Initial setup of personal website with React and Tailwind CSS

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="bg-black min-h-screen w-full m-0 p-0">
+      {/* Completely black webpage */}
+    </div>
+  );
+}
+
+export default App;
